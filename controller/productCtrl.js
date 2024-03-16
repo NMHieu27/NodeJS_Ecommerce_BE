7cd0f977dcc557e06ced901cffe48d8da1b6a3f7
MHieu27 <nguyenminhhieu271201@gmail.com>
2024-03-16 15:15:09 +0700

    funct: update and delete product

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -5,51 +5,68 @@ const validateMongoDbId = require("../utils/validateMongodbId");
 const { Error } = require("mongoose");
 
 // Create a new product
-const createProduct = asyncHandler( async (req, res) => {
-  try{
-    const product = await Product.create(req.body)
-    res.json({product})
-  }
-  catch (err){
-    throw new Error('Error creating product:', err);
+const createProduct = asyncHandler(async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.json({ product });
+  } catch (err) {
+    throw new Error("Error creating product:", err);
   }
 });
 // Get all products
-const getAllProducts = asyncHandler( async (req, res) => {
-  try{
+const getAllProducts = asyncHandler(async (req, res) => {
+  try {
     const products = await Product.find();
-    res.json(products)
-  }
-  catch (err){
-    throw new Error('Error getting all products:', err);
+    res.json(products);
+  } catch (err) {
+    throw new Error("Error getting all products:", err);
   }
 });
 // Get a product by slug
-const getProductBySlug = asyncHandler( async (req, res) =>{
-  try{
-    const product = await Product.findOne({slug:req.params.slug});
-    if(product){
+const getProductBySlug = asyncHandler(async (req, res) => {
+  try {
+    const product = await Product.findOne({ slug: req.params.slug });
+    if (product) {
       res.json(product);
-    }
-    else throw new Error('Slug invalid');
-  }
-  catch(err){
-    throw new Error('Error getting product by slug:', err);
+    } else throw new Error("Slug invalid");
+  } catch (err) {
+    throw new Error("Error getting product by slug:", err);
   }
 });
 
 // Get a product by slug
-const getProductById = asyncHandler( async (req, res) =>{
+const getProductById = asyncHandler(async (req, res) => {
+  try {
+    const { id } = req.params;
+    validateMongoDbId(id);
+    const product = await Product.findById(id);
+    if (product) {
+      res.json(product);
+    } else throw new Error("Product not found");
+  } catch (err) {
+    throw new Error(err);
+  }
+});
+//Update a product by id
+const updateProduct = asyncHandler(async (req, res) => {
+  try {
+    const { id } = req.params;
+    validateMongoDbId(id);
+    const product = await Product.findOneAndUpdate({ _id: id }, req.body,{new: true});
+    res.json({message : 'Product updated successfully'});
+  } catch (err) {
+    throw new Error(err);
+  }
+});
+// Delete a product by id
+const deleteProduct = asyncHandler(async (req, res) =>{
   try{
     const {id} = req.params;
     validateMongoDbId(id);
-    const product = await Product.findById(id);
-    if(product){
-      res.json(product);
-    }
-    else throw new Error('Product not found');
+    const productDelete = await Product.findOneAndDelete({ _id: id });
+    res.json({message : 'Product deleted successfully'});
   }
-  catch(err){
+  catch (err) {
     throw new Error(err);
   }
 });
@@ -59,4 +76,6 @@ module.exports = {
   getAllProducts,
   getProductBySlug,
   getProductById,
+  updateProduct,
+  deleteProduct,
 };
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,11 +1,20 @@
 const express = require("express");
-const { createProduct, getAllProducts, getProductBySlug, getProductById } = require("../controller/productCtrl");
+const {
+  createProduct,
+  getAllProducts,
+  getProductBySlug,
+  getProductById,
+  updateProduct,
+  deleteProduct,
+} = require("../controller/productCtrl");
 const { isAdmin, authMiddleware } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
-router.post('/', createProduct);
-router.get('/', getAllProducts);
-router.get('/:id', getProductById);
-router.get('/slug/:slug', getProductBySlug);
+router.post("/", createProduct);
+router.get("/", getAllProducts);
+router.get("/:id", getProductById);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
+router.get("/slug/:slug", getProductBySlug);
 
 module.exports = router;
